78ba79a40a8d3a0eb972bbccbfad14ba8d38ab8c
Kevin Roney <ktroney.pdx@gmail.com>
2022-04-26 16:39:06 -0700

    finished AnimalList.js and css

---
+++ src/AnimalList/AnimalList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AnimalList';
+import Animal from '../Animal/Animal.js';
+
+export default function AnimalList({ animals }) {
+  return (
+    <main>
+      animals.map((animal) =>
+      <Animal 
+        key={animal.name} 
+        name={animal.name}
+        type={animal.type}
+        says={animal.says}
+      />)
+    </main>
+  );
+}
