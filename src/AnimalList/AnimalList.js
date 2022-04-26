1f3111cf8bb46799405e10db6631111e2ba102d4
Kevin Roney <ktroney.pdx@gmail.com>
2022-04-26 16:41:31 -0700

    fixed .map function by wrapping it {}

--- src/AnimalList/AnimalList.js
+++ src/AnimalList/AnimalList.js
@@ -5,13 +5,15 @@ import Animal from '../Animal/Animal.js';
 export default function AnimalList({ animals }) {
   return (
     <main>
-      animals.map((animal) =>
-      <Animal 
-        key={animal.name} 
-        name={animal.name}
-        type={animal.type}
-        says={animal.says}
-      />)
+      {
+        animals.map((animal) =>
+          <Animal 
+            key={animal.name} 
+            name={animal.name}
+            type={animal.type}
+            says={animal.says}
+          />)
+      }
     </main>
   );
 }
