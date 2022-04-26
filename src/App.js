c89a416d1c4807237325a856b16514ed732411b3
Kevin Roney <ktroney.pdx@gmail.com>
2022-04-26 16:23:08 -0700

    finished App.js, Footer.js and Header.js and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header/Header.js';
+import Main from './Main/Main.js';
+import Footer from './Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header greeting="Welcome to the Farm!"/>
+      <Main/>
+      <Footer email="ktroney@gmail.com"/>
     </div>
   );
 }
