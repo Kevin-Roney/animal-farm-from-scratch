57e4f08ca0d5a387caad461c0db0264916070070
Kevin Roney <ktroney.pdx@gmail.com>
2022-04-26 18:04:42 -0700

    changed test paramets for npm test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Main from '../src/Main/Main.js';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(Main).toBeInTheDocument();
+  const linkElement = screen.getByText(/You have been my friend/i);
+  expect(linkElement).toBeInTheDocument();
 });
